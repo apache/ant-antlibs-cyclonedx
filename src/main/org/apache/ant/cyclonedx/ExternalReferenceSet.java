90ce8d9665716cfaf588ba2aa4efa3e65ec88fc5
Stefan Bodewig <bodewig@apache.org>
2026-05-13 06:11:02 +0200

    document externalreferenceset

--- src/main/org/apache/ant/cyclonedx/ExternalReferenceSet.java
+++ src/main/org/apache/ant/cyclonedx/ExternalReferenceSet.java
@@ -6,15 +6,26 @@ import java.util.List;
 
 import org.apache.tools.ant.types.DataType;
 
+/**
+ * A container for a collection of {@link ExternalReference}s.
+ *
+ * <p>This class is a type exposed by this Ant Library. When using the
+ * inherited {@code refid} attribute it can reference an instance
+ * defined previously - in which case no child elements are
+ * allowed.</p>
+ */
 public class ExternalReferenceSet extends DataType {
     private List<org.cyclonedx.model.ExternalReference> externalReferences = new ArrayList<>();
 
+    /**
+     * Adds an external reference to this set.
+     */
     public void addConfiguredExternalReference(ExternalReference ref) {
         checkChildrenAllowed();
         externalReferences.add(ref.toCycloneDxExternalReference());
     }
 
-    public Collection<org.cyclonedx.model.ExternalReference> getExternalReferences() {
+    Collection<org.cyclonedx.model.ExternalReference> getExternalReferences() {
         if (isReference()) {
             return getRef().getExternalReferences();
         }
@@ -24,8 +35,8 @@ public class ExternalReferenceSet extends DataType {
 
     /**
      * Perform the check for circular references and return the
-     * referenced Organization.
-     * @return <code>Organization</code>.
+     * referenced ExternalReferenceSet.
+     * @return <code>ExternalReferenceSet</code>.
      */
     protected ExternalReferenceSet getRef() {
         return getCheckedRef(ExternalReferenceSet.class);
