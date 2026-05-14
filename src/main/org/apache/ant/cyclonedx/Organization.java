57a2261dda2654a045735385891cbaf8951fbbd7
Stefan Bodewig <bodewig@apache.org>
2026-05-14 15:23:28 +0200

    document and test organization type

--- src/main/org/apache/ant/cyclonedx/Organization.java
+++ src/main/org/apache/ant/cyclonedx/Organization.java
@@ -8,21 +8,39 @@ import org.apache.tools.ant.types.resources.URLResource;
 
 import org.cyclonedx.model.OrganizationalEntity;
 
+/**
+ * Organization appears as "manufacturer", "publisher" or "supplier"
+ * of components or the SBOM itself.
+ *
+ * <p>The CycloneDX specification supports more information for an
+ * organization than this type currently exposes.</p>
+ *
+ * <p>This class is a type exposed by this Ant Library. When using the
+ * inherited {@code refid} attribute it can reference an instance
+ * defined previously - in which case no child elements or other
+ * attributes are allowed.</p>
+ */
 public class Organization extends DataType {
     private String name;
     private List<String> urls = new ArrayList<>();
 
+    /**
+     * Sets the name of the organization.
+     */
     public void setName(String name) {
         checkAttributesAllowed();
         this.name = name;
     }
 
+    /**
+     * Adds an url of the organization.
+     */
     public void addConfiguredUrl(URLResource url) {
-        checkAttributesAllowed();
+        checkChildrenAllowed();
         urls.add(url.getURL().toExternalForm());
     }
 
-    public OrganizationalEntity toOrganizationalEntity() {
+    OrganizationalEntity toOrganizationalEntity() {
         if (isReference()) {
             return getRef().toOrganizationalEntity();
         }
@@ -37,7 +55,10 @@ public class Organization extends DataType {
         return oe;
     }
 
-    public static Organization from(OrganizationalEntity oe) {
+    /**
+     * Creates a new instance from the CycloneDX counterpart.
+     */
+    static Organization from(OrganizationalEntity oe) {
         Organization o = new Organization();
         o.setName(oe.getName());
         List<String> urls = oe.getUrls();
